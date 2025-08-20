460928a4452eef5c485c7e887790c5a73b1a6abe
Igor Bittencourt <moreira_igor9@hotmail.com>
2025-08-20 19:56:03 -0300

    Lista1Exercicio1

---
+++ Lista 1/exec1/src/main/java/com/devigode/Pessoa.java
@@ -0,0 +1,21 @@
+package com.devigode;
+
+public class Pessoa {
+    private String nome;
+    private double peso;   
+    private double altura;
+    private String sexo;
+
+    public Pessoa(String nome, double peso, double altura, String sexo) {
+        this.nome = nome;
+        this.peso = peso;
+        this.altura = altura;
+        this.sexo = sexo;
+    }
+    public double calcularIMC() {
+        if (altura <= 0) {
+            throw new IllegalArgumentException("Altura deve ser maior que zero.");
+        }
+        return peso / (altura * altura);
+    }
+}
